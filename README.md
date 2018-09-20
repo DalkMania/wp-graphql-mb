79514440065357e244f8a12a4ae4a820c7ed6af2
Niklas Dahlqvist <dalkmania@gmail.com>
2018-09-20 15:52:48 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+WP GraphQL Meta Box Custom Fields
+===============
+
+This plugin is an add-on for the awesome [WP GraphQL][wp-graphql] & [a WordPress custom fields and custom meta boxes][metabox.io]
+
+It builds on top of WP GraphQL and adds all registered metaboxes to the WP GraphQL Endpoint.
+
+## Installing
+
+1. Make sure that [WP GraphQL][wp-graphql] is installed and activated.
+2. Make sure that [Metabox.io WordPress plugin][metabox.io] is installed and activated 
+3. Upload this repo (or git clone) to your plugins folder and activate it.
+
+## Usage
+
+Just activate the plugin and the data from your metaboxes will be avialble to you at the WP GraphQL Endpoint for you to query.
+
+[wp-graphql]: https://github.com/wp-graphql/wp-graphql
+[metabox.io]: https://metabox.io
---
+++ wp-graphql-mb.php
@@ -0,0 +1,227 @@
+<?php
+/**
+ * Plugin Name: WP GraphQL Meta Box Custom Fields
+ * Description: Exposes all registered Meta Box Custom Fields to the WPGraphQL EndPoint.
+ * Author: Niklas Dahlqvist
+ * Author URI: https://www.niklasdahlqvist.com
+ * Version: 0.5
+ * License: GPL2+
+ */
+
+namespace WPGraphQL\Extensions;
+
+use WPGraphQL\Types;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+if (!class_exists('\WPGraphQL\Extensions\MB')) {
+    class MB
+    {
+        /**
+        * List of media fields to filter.
+        *
+        * @var array
+        */
+        protected $media_fields = [
+            'media',
+            'file',
+            'file_upload',
+            'file_advanced',
+            'image',
+            'image_upload',
+            'image_advanced',
+            'plupload_image',
+            'thickbox_image',
+        ];
+
+        public function __construct()
+        {
+            $this->add_meta_boxes_to_graphQL();
+        }
+
+        public function add_meta_boxes_to_graphQL()
+        {
+            $types = array_merge(
+              $this->get_types(),
+              $this->get_types('taxonomy')
+            );
+
+            foreach ($types as $type => $object) {
+                if (isset($object->graphql_single_name)) {
+                    $graphql_type = $object->graphql_single_name;
+
+                    add_filter("graphql_{$graphql_type}_fields", function ($fields) use ($type) {
+                        return $this->add_meta_fields($fields, $type);
+                    });
+                }
+            }
+        }
+
+        public function add_meta_fields($fields, $object_type)
+        {
+            $boxes = array_merge(
+              $this->get_post_type_meta_boxes($object_type),
+              $this->get_term_meta_boxes($object_type)
+            );
+
+            foreach ($boxes as $box) {
+                foreach ($box->fields as $field) {
+                    $field_name = self::_graphql_label($field['id']);
+
+                    if (!in_array($field['type'], $this->media_fields) && $field['clone'] == false && $field['multiple'] == false) {
+                        $fields[$field_name] = [
+                            'type' => Types::string(),
+                            'description' => $field['desc'],
+                            'resolve' => function ($object) use ($object_type, $field) {
+                                if ('post' === $object_type || in_array($object_type, get_post_types(), true)) {
+                                    return !empty(get_post_meta($object->ID, $field['id'], true)) ? get_post_meta($object->ID, $field['id'], true) : null;
+                                }
+                                if ('term' === $object_type || in_array($object_type, get_taxonomies(), true)) {
+                                    return !empty(get_term_meta($object->term_id, $field['id'], true)) ? get_term_meta($object->term_id, $field['id'], true) : null;
+                                }
+                                if ('user' === $object_type) {
+                                    return !empty(get_user_meta($object->ID, $field['id'], true)) ? get_user_meta($object->ID, $field['id'], true) : null;
+                                }
+                            },
+                        ];
+                    }
+                    if (!in_array($field['type'], $this->media_fields) && ($field['clone'] == true || $field['multiple'] == true)) {
+                        $fields[$field_name] = [
+                            'type' => Types::list_of(Types::string()),
+                            'description' => $field['desc'],
+                            'resolve' => function ($object) use ($object_type, $field) {
+                                if ('post' === $object_type || in_array($object_type, get_post_types(), true)) {
+                                    return !empty(get_post_meta($object->ID, $field['id'], true)) ? get_post_meta($object->ID, $field['id'], true) : null;
+                                }
+                                if ('term' === $object_type || in_array($object_type, get_taxonomies(), true)) {
+                                    return !empty(get_term_meta($object->term_id, $field['id'], true)) ? get_term_meta($object->term_id, $field['id'], true) : null;
+                                }
+                                if ('user' === $object_type) {
+                                    return !empty(get_user_meta($object->ID, $field['id'], true)) ? get_user_meta($object->ID, $field['id'], true) : null;
+                                }
+                            },
+                        ];
+                    }
+                    if (in_array($field['type'], $this->media_fields)) {
+                        $fields[$field_name] = [
+                            'type' => Types::list_of(Types::post_object('attachment')),
+                            'description' => $field['desc'],
+                            'resolve' => function ($object) use ($object_type, $field) {
+                                if ('post' === $object_type || in_array($object_type, get_post_types(), true)) {
+                                    $values = !empty(get_post_meta($object->ID, $field['id'], false)) ? get_post_meta($object->ID, $field['id'], false) : null;
+                                }
+                                if ('term' === $object_type || in_array($object_type, get_taxonomies(), true)) {
+                                    $values = !empty(get_term_meta($object->term_id, $field['id'], false)) ? get_term_meta($object->term_id, $field['id'], false) : null;
+                                }
+                                if ('user' === $object_type) {
+                                    $values = !empty(get_user_meta($object->ID, $field['id'], false)) ? get_user_meta($object->ID, $field['id'], false) : null;
+                                }
+
+                                if ($values != null) {
+                                    $images = [];
+                                    foreach ($values as $value) {
+                                        $images[] = \WP_Post::get_instance($value);
+                                    }
+                                    return $images;
+                                } else {
+                                    return null;
+                                }
+                            },
+                        ];
+                    }
+                }
+            }
+
+            return $fields;
+        }
+
+        /**
+         * Get metaboxes .
+         *
+         * @param array $object Post object.
+         *
+         * @return array
+         */
+        public function get_post_type_meta_boxes($type)
+        {
+            $meta_boxes = \rwmb_get_registry('meta_box')->get_by(['object_type' => 'post']);
+            foreach ($meta_boxes as $key => $meta_box) {
+                if (!in_array($type, $meta_box->post_types, true)) {
+                    unset($meta_boxes[$key]);
+                }
+            }
+            return $meta_boxes;
+        }
+
+        /**
+         * Get term meta boxes.
+         *
+         * @param array $object Term object.
+         *
+         * @return array
+         */
+        public function get_term_meta_boxes($type)
+        {
+            $output = [];
+            if (!class_exists('MB_Term_Meta_Box')) {
+                return $output;
+            }
+
+            $meta_boxes = \rwmb_get_registry('meta_box')->get_by([
+                'object_type' => 'term',
+            ]);
+            foreach ($meta_boxes as $key => $meta_box) {
+                if (!in_array($type, $meta_box->taxonomies, true)) {
+                    unset($meta_boxes[$key]);
+                }
+            }
+
+            return $meta_boxes;
+        }
+
+        /**
+         * Get supported supported post types and / or taxonomies.
+         *
+         * @param string $type 'post' or 'taxonomy'.
+         *
+         * @return array
+         */
+        protected function get_types($type = 'post')
+        {
+            $types = get_post_types([], 'objects');
+            if ('taxonomy' === $type) {
+                $types = get_taxonomies([], 'objects');
+            }
+            foreach ($types as $type => $object) {
+                if (empty($object->show_in_rest)) {
+                    unset($types[$type]);
+                }
+            }
+
+            return $types;
+        }
+
+        /**
+         * Utility function for formatting a string to be compatible with GraphQL labels (camelCase with lowercase first letter)
+         *
+         * @param $input
+         *
+         * @return mixed|string
+         */
+        public static function _graphql_label($input)
+        {
+            $graphql_label = str_ireplace('_', ' ', $input);
+            $graphql_label = ucwords($graphql_label);
+            $graphql_label = str_ireplace(' ', '', $graphql_label);
+            $graphql_label = lcfirst($graphql_label);
+
+            return $graphql_label;
+        }
+    }
+}
+
+add_action('do_graphql_request', function () {
+    new MB;
+});
